2a2b4acc027890a6ef835116dbc70978245158d3
James Thompson <jtbb58@googlemail.com>
2018-06-12 16:29:45 +0100

    Update InstallWSUS_v1.1.ps1

--- PS_Scripts/InstallWSUS_v1.1.ps1
+++ PS_Scripts/InstallWSUS_v1.1.ps1
@@ -118,6 +118,106 @@ else{
         $rule.Enabled = $true
         $rule.ApplyRule()
 
+#Create New Rule Object
+
+$newRule = $wsus.CreateInstallApprovalRule("2012Servers")
+
+ 
+
+##Categories
+
+#Get Categories for Windows Server
+
+$updateCategories = $wsus.GetUpdateCategories() | Where {
+
+  $_.Title -LIKE "Windows Server 2012*"
+
+}
+
+ 
+
+#Create collection for Categories
+
+$categoryCollection = New-Object Microsoft.UpdateServices.Administration.UpdateCategoryCollection
+
+$categoryCollection.AddRange($updateCategories)
+
+ 
+
+#Add the Categories to the Rule
+
+$newRule.SetCategories($categoryCollection)
+
+ 
+
+##Classifications
+
+#Get all Classifications for specific Classifications
+
+$updateClassifications = $wsus.GetUpdateClassifications() | Where {
+
+  $_.Title -Match "Critical Updates|Service Packs|Updates|Security Updates"
+
+}
+
+ 
+
+#Create collection for Categories
+
+$classificationCollection = New-Object Microsoft.UpdateServices.Administration.UpdateClassificationCollection
+
+$classificationCollection.AddRange($updateClassifications )
+
+ 
+
+#Add the Classifications to the Rule
+
+$newRule.SetUpdateClassifications($classificationCollection)
+
+ 
+
+##Target Groups
+
+#Get Target Groups required for Rule
+
+$targetGroups = $wsus.GetComputerTargetGroups() | Where {
+
+  $_.Name -Match "All Computers"
+
+}
+
+ 
+
+#Create collection for TargetGroups
+
+$targetgroupCollection = New-Object Microsoft.UpdateServices.Administration.ComputerTargetGroupCollection
+
+$targetgroupCollection.AddRange($targetGroups)
+
+ 
+
+#Add the Target Groups to the Rule
+
+$newRule.SetComputerTargetGroups($targetgroupCollection)
+
+ 
+
+#Finalize the creation of the rule object
+
+$newRule.Enabled = $True
+
+$newRule.Save()
+
+ 
+
+#Run the rule
+
+$newRule.ApplyRule()
+
+ 
+
+Let’s make sure that the rule is now created.
+
         Write-Verbose "Configure the Classifications" -Verbose
 
         Get-WsusClassification | Where-Object {
